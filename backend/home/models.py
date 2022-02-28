23a847523ece480993c45fc86814b2f3a4fa0fbc
Crowdbotics <team@crowdbotics.com>
2022-02-28 05:51:40 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,25 @@
+from django.conf import settings
 from django.db import models
+
+
+class Approval(models.Model):
+    "Generated Model"
+    shape = models.TextField()
+    size = models.DecimalField(
+        max_digits=3,
+        decimal_places=2,
+    )
+    color = models.TextField()
+    clarity = models.TextField()
+    rap = models.IntegerField()
+    discount = models.SmallIntegerField()
+    price = models.BigIntegerField()
+    approval = models.TextField()
+    date = models.DateTimeField(
+        auto_now=True,
+    )
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="approval_user",
+    )
